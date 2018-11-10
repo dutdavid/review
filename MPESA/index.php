0b8716f9adcc97c2a4650b0d468a2c15da6fc8c4
David Dut <35602624+dutdavid@users.noreply.github.com>
2018-11-10 18:35:40 +0300

    added all files

---
+++ MPESA/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>iLand Multipurpose Landing Page Template</title>
+<link rel="icon" href="images/favicon.png" type="image/png" sizes="16x16">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="iLand Multipurpose Landing Page Template">
+<meta name="keywords" content="iLand HTML Template, iLand Landing Page, Landing Page Template">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+<link rel="stylesheet" href="css/animate.css">
+<!-- Resource style -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/ionicons.min.css">
+<!-- Resource style -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div class="wrapper">
+  <div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container"> 
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+         
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+         <a href="../index.html" role="button" class="btn btn-primary btn-lg">HOME</a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid -->
+  
+  <div class="main app form" id="main"><!-- Main Section-->
+    <div class="hero-section">
+      <div class="container nopadding">
+        <div class="col-md-12">
+          <div class="hero-content text-center">
+            
+        </div>
+      </div>
+    </div>
+    <!-- Classes Section -->
+   
+
+
+    <!-- Package Section -->
+    <style>
+      .pricing p {
+  font-size: 28px;
+  font-weight: 400;
+  letter-spacing: -1px;
+  line-height: 1.2;
+}
+
+    </style>
+    <div id="pricing" class="pricing-section text-center">
+      <div class="container">
+        <div class="col-md-12 col-sm-12 nopadding">
+          <div class="pricing-intro">
+            <h1 class="wow fadeInUp" data-wow-delay="0s"><bold>PAYMENT DETAILS<bold></h1>
+            <p><h1>Mpesa Till 766500</h1></p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    
+    
+    
+   
+  <!-- Scroll To Top --> 
+  
+
+  
+  <!-- Scroll To Top Ends--> 
+  
+</div>
+<!-- Main Section -->
+</div>
+<!-- Wrapper--> 
+
+<!-- Jquery and Js Plugins --> 
+<script type="text/javascript" src="js/jquery-2.1.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="js/plugins.js"></script> 
+<script type="text/javascript" src="js/menu.js"></script> 
+<script type="text/javascript" src="js/custom.js"></script> 
+<script src="js/jquery.subscribe.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!-- Metas Page details-->
+<title>DIARY OF A POULTRY FARMER</title>
+<meta name="description" content="OnePage Resume Portfolio">
+<meta name="author" content="">
+<!-- Mobile Specific Metas-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--main style-->
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
+<!--google font style-->
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
+<!--font-family: 'Metrophobic', serif;-->
+<link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,600,400italic,600italic' rel='stylesheet' type='text/css'> 
+<!--font-family: 'Open Sans', sans-serif;-->
+<!-- font icon css style-->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+</head>
+<body onLoad="load()" onUnload="GUnload()">
+<!-- Preloader -->
+<div id="preloader">
+	<div id="status"></div>
+</div>
+
+<!--wrapper start-->
+<div class="wrapper noGap" id="wrapper">
+
+<!--Header start -->
+<header>
+  	<!--menu start-->
+    <div class="menu">
+    <a href="#" class="nav-icon" id="nav-show"><i class="fa fa-bars"></i></a>
+      <div class="navbar-wrapper">
+        <div class="container">
+          <div class="navwrapper">
+            <div class="navbar navbar-inverse navbar-static-top">
+              <div class="container">
+              	<!--<div class="logo">logo</div> -->
+                <div class="navArea"><a href="#" class="closeMenu"><i class="fa fa-times"></i></a>
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                     <!-- <a class="navbar-brand" href="#">Menu</a>  --></div>
+                    <div class="navbar-collapse collapse">
+                      <ul class="nav navbar-nav">
+                        <li class="menuItem active"><a href="#wrapper">Home</a></li>
+                        <li class="menuItem"><a href="#aboutus">Author</a></li>
+                        <li class="menuItem"><a href="#skillset">Book Sumarry</a></li>
+                        <li class="menuItem"><a href="#contact">Contact</a></li>
+                      </ul>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- End Navbar -->
+        </div>
+      </div>
+    </div>
+    <!--menu end--> 
+    <!--banner start-->
+    <div class="banner row" id="banner">
+    <div class="bannerText">
+        <h1>DIARY OF A POULTRY FARMER</h1>
+        <h3>LEARN HOW TO TRANSFORM THE SCIENCE AND ART<br>OF POULTRY FARMING INTO A THRIVING BUSINESS</h3>
+      </div>
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 noPadd slides-container" style="height:50%"> 
+        <!--background slide show start-->
+        <div class="slide"> 
+          <img src="images/header-image/cover.jpg" alt="image01"> </div>
+        <div class="slide"> 
+          <img src="images/header-image/cover2.jpg" alt="image01"> </div>
+        <!--background slide show end--> 
+      </div>
+    </div>
+    <!--banner end--> 
+    
+  </header>
+  <!--Header end -->
+  <section class="protfolio" id="ourwork">
+    <div class="container">
+        <div class="heading">
+            <h2>COVER PICTURES</h2>
+            <h3></h3>            
+        </div>
+    </div>
+    <div class="row">
+      <div class="portfolioFilter">
+        
+      </div>
+      <ul class="portfolioContainer row">
+      <li class="template col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="cover.jpg" title="Project Title2" class="zoom lb lb_warsaw1"><i class="fa fa-plus fa-2x"></i></a> </span> </span> <img src="cover.jpg" alt="" > </div>
+        </li>
+        <li class="template wordpress illustration col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="cover.jpg" title="Project Title2" class="zoom lb lb_warsaw1"><i class="fa fa-search fa-2x"></i></a>  </span> </span> <img src="cover2.jpg" alt="" > </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+ <!--about us start-->
+  <section class="aboutus" id="aboutus">
+    <div class="container">
+      <div class="row">
+       <div class="col-md-12">
+      	<div class="heading">
+        <h2>About me</h2>
+        <h3>A small introduction about my self</h3>        
+      </div>
+      </div> 
+       </div> 
+      <div class="row">
+      <div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right media"><img src="images/bio.jpeg" alt=""></div>
+      	<div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left media">
+        	<h5>Professor Bitange Ndemo</h5>
+            <h4>School of Business, University of Nairobi.</h4>
+            <p>Dr. Subiri Obwogo is a medical doctor,specialist in pulic health medicine,Chevening Scholar,improvement advisor, farmer and consultant in health policy and systems strengthening.He has a Bachelor of Medicine and bachelor of surgery degrees from University of Nairobi and a Master of Science in Public Health Medicine from University of Nottingham school Epidemology and Public Health Medicine in the United Kingdom.</p><p>He is the founder of kienyeji Kenya Ltd,a free-range poultry social enterprise and a knowledge management start-up that uses storytelling to foster learning and to build entrepreneurial knowledge and skills.He also offers poultry farming training.Dr Obwogo is also author of <italics> The Bombs That Shook Nairobi and Dar:A Story of Pain and Betrayal (1999).<italics>
+</p>
+<p><a role="button" class="btn btn-primary btn-lg">VIEW BLOG</a></p>
+        </div>
+      </div>     
+    </div>    
+  </section>
+  <!--about us end--> 
+  <!--Skillset start -->
+  <section id="skillset" class="skillset">
+  	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12">
+               <div class="heading">
+                <h2>Book Summary</h2>       
+              </div>
+            </div>
+            <div class="col-md-12">
+            	<div class="col-md-12 col-sm-12 media">
+  <p>The Diary of a Poultry Farmer is a popular column authored by Dr Subiri Obwongo and published weekly in the Seeds
+ of Gold pull-out by the Nation Media Group. In the last three years of its existence, Dr Obwongo has shared practically useful,
+ scientifically-based information in a simpler way farmers can pick lessons.
+Readers look forward to his daily captivating first-person experiences in poultry husbandry, agri-preneurship and
+the practical solutions. They yearn for the disarming candour of the Diary. But overtime,
+the various information nuggets have become scattered over countless editions necessitating
+a relentless and often annoying search even for the most ardent fans with the best memories,hence this guide.
+<br>
+The illustrated Practical Guide of the Diary is a comprehensive step by step one-stop reference
+on all practical and technical titbits from previous editions. It covers a wide range
+of topics that appeal to farmers, hobbyists,policy-makers, researchers, students and
+institutions of higher learning. The lessons are delivered as a series of stories. It's witty,
+insightful, coversational and includes the ever-refresshing readers's questions and their razor-sharp comments.
+If you love a good story, read on. The Diary is now in your hands.<p>
+<p><a href="MPESA/index.html" role="button" class="btn btn-primary btn-lg">PURCHASE BOOK</a></p>
+
+                </div>             
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
+  <!--Skillset end -->
+ 
+  <!--portfolio start-->
+	<section class="protfolio" id="ourwork">
+    <div class="container">
+        <div class="heading">
+            <h2>Portfolio</h2>
+            <h3>What I have done in my academic career</h3>            
+        </div>
+    </div>
+    <div class="row">
+      <div class="portfolioFilter">
+        
+      </div>
+      <ul class="portfolioContainer row">
+      <li class="template col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="images/projects/big-images/Diary_Pics insert-1.jpg" title="Project Title2" class="zoom lb lb_warsaw1"><i class="fa fa-plus fa-2x"></i></a> </span> </span> <img src="images/projects/big-images/Diary_Pics insert-1.jpg" alt="" > </div>
+        </li>
+        <li class="template wordpress illustration col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="images/projects/big-images/Diary_Pics insert-2.jpg" title="Project Title2" class="zoom lb lb_warsaw1"><i class="fa fa-search fa-2x"></i></a>  </span> </span> <img src="images/projects/big-images/Diary_Pics insert-2.jpg" alt="" > </div>
+        </li>
+        <li class="wordpress illustration col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="images/projects/big-images/Diary_Pics insert-3.jpg" title="Project Title3" class="zoom lb lb_warsaw1"><i class="fa fa-search fa-2x"></i></a>  </span> </span> <img src="images/projects/big-images/Diary_Pics insert-3.jpg" alt="" > </div>
+        </li>
+        <li class="webdesign wordpress col-xs-6 col-sm-4 col-md-3 col-lg-3">
+          <div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="images/projects/big-images/Diary_Pics insert-4.jpg" title="Project Title4" class="zoom lb lb_warsaw1"><i class="fa fa-search fa-2x"></i></a>  </span> </span> <img src="images/projects/big-images/Diary_Pics insert-4.jpg" alt="" > </div>
+        </li>
+       
+    
+  </section>
+  <!--portfolio end-->   
+  
+  
+  <!--footer start-->
+  <section class="footer" id="footer">
+    <div class="container">
+      <ul>
+        <li><a href="www.twitter.com/@kienyejikenya.com"><i class="fa fa-twitter fa-2x"></i></a></li>
+        <li><a href="https://mobile.facebook.com/KienyejiKenya/?notif_t=page_&_rdc=1&_rdr"><i class="fa fa-facebook fa-2x"></i></a></li>
+        <li><a href="www.kienyejikenya.com"><i class="fa fa-google fa-2x"></i></a></li>
+       
+      </ul>
+      <P>P.O. Box 6120-00300<br>Nairobi Kenya<br>0714378596</P>>
+    </div>
+    <p class="text-center"><a href="#wrapper" class="gototop"><i class="fa fa-chevron-up fa-2"></i></a></p>
+  </section>
+  <!--footer end-->     
+  </div>
+<!--wrapper end--> 
+
+<!--modernizr js--> 
+<script type="text/javascript" src="js/modernizr.custom.26633.js"></script> 
+<!--jquary min js--> 
+<script type="text/javascript" src="js/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+
+<!--for placeholder jquery--> 
+<script type="text/javascript" src="js/jquery.placeholder.js"></script> 
+
+<!--for header jquery--> 
+<script type="text/javascript" src="js/stickUp.js"></script> 
+<script src="js/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+jQuery(function($) {
+$(document).ready( function() {
+  //enabling stickUp on the '.navbar-wrapper' class
+	$('.navbar-wrapper').stickUp({
+		parts: {
+		  0: 'banner',
+		  1: 'aboutus',
+		  2: 'skillset',
+		  3: 'experience',
+		  4: 'education',
+		  5: 'ourwork',
+		  6: 'contact'
+		},
+		itemClass: 'menuItem',
+		itemHover: 'active',
+		topMargin: 'auto'
+		});
+	});
+	
+
+});
+</script>
+<script>
+	$('#banner').superslides({
+	  animation: 'fade',
+	  play: 5000
+	});
+</script>  
+
+<!--for portfolio jquery--> 
+<script src="js/jquery.isotope.js" type="text/javascript"></script> 
+<link type="text/css" rel="stylesheet" id="theme" href="css/jquery-ui-1.8.16.custom.css">
+<link type="text/css" rel="stylesheet" href="css/lightbox.min.css">
+<script type="text/javascript" src="js/jquery.ui.widget.min.js"></script> 
+<script type="text/javascript" src="js/jquery.ui.rlightbox.js"></script> 
+
+<!--contact form js--> 
+<script type="text/javascript" src="js/jquery.contact.js"></script> 
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+<script type="text/javascript">
+        </script>
+
+<script src="js/jquery.easing.js"></script> 
+<script src="js/jquery.mousewheel.js"></script> 
+<script defer src="js/slideroption.js"></script> 
+
+<!--for theme custom jquery--> 
+<script src="js/custom.js"></script>
+
+</body>
+</html>
